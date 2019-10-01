ac4210cbeb88b3089f9136105c1cd0ee0f17c23a
Mohit23x <mohit@smilebots.com>
2019-10-01 12:01:45 +0530

    added test.js

---
+++ test.js
@@ -0,0 +1,23 @@
+const slugit = require('./index');
+
+console.log("\nHindi");
+console.log(slugit("हैलो वर्ल्ड यह सैंपल टेस्ट है"));
+
+console.log("\nUrdu");
+console.log(slugit("ہیلو ورلڈ یہ نمونہ ٹیسٹ ہے"));
+
+console.log("\nTamil");
+console.log(slugit("ஹலோ வேர்ல்ட் இது மாதிரி சோதனை"));
+
+console.log("\nPunjabi");
+console.log(slugit("ਹੈਲੋ ਵਰਲਡ ਇਹ ਨਮੂਨਾ ਟੈਸਟ ਹੈ"));
+
+console.log("\nMalayalam");
+console.log(slugit("ഹലോ വേൾഡ് ഇതാണ് സാമ്പിൾ ടെസ്റ്റ്"));
+
+console.log("\nBengali");
+console.log(slugit("হ্যালো ওয়ার্ল্ড এটি নমুনা পরীক্ষা"));
+
+console.log("\nEnglish");
+console.log(slugit("Hello World, this is a sample test"));
+
