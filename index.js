2334eaea9064aaa4e07817a952f1fe18a4d0b37c
Mohit23x <mohit@smilebots.com>
2019-10-01 11:44:55 +0530

    function working

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 function slugit(str){
-    str.replace(/[`~!@#$%^&*()_\-+=\[\]{};:'"\\|\/,.<>?\s]/g, " ").toLowerCase();
-    str.replace(/^\s+|\s+$/gm, "");
-    str.replace(/\s+/g, "-").trim();
-    return str.toLowerCase();
-
+ let t1 = str.replace(/[`~!@#$%^&*()_\-+=\[\]{};:'"\\|\/,.<>?\s]/g, " ").toLowerCase();
+ let t2 = t1.replace(/^\s+|\s+$/gm, "");
+ var t3 = t2.replace(/\s+/g, "-").trim();
+ return t3.toLowerCase();
 }
 
 module.exports = slugit
